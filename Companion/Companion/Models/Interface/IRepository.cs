f1697571411f77b3a3475e744f4ed6a67d6868b1
DmitriiPodlev <42978023+DmitriiPodlev@users.noreply.github.com>
2021-04-29 17:58:12 +0300

    Update Database

---
+++ Companion/Companion/Models/Interface/IRepository.cs
@@ -0,0 +1,31 @@
+﻿using Companion.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Companion.Models.Interface
+{
+    public interface IRepository
+    {
+        Task<IdentityResult> CreateUser(User user, string password = null);
+        Task<User> FindUser(string userId);
+        Task<IdentityResult> UpdateUser(User user);
+        Task<IdentityResult> DeleteUser(User user);
+        Task<User> FindUserByName(string name);
+        Task AddLogin(User user, ExternalLoginInfo info);
+        List<User> GetUsers();
+        Task<User> GetUser(ClaimsPrincipal user);
+        Task CreateActivity(Activity activity);
+        Task DeleteAcivity(int id);
+        Activity GetActivity(int id);
+        List<Activity> GetActivities();
+        Task CreateConfig(Config config);
+        Task CreateService(Service service);
+        Task CreateLocation(Location location);
+        Location GetLocation(int id);
+        List<Location> GetLocations();
+    }
+}
--- Companion/Companion/Models/Interface/Repository.cs
+++ Companion/Companion/Models/Interface/Repository.cs
@@ -1,12 +1,122 @@
-﻿using System;
+﻿using Companion.Models.Context;
+using Companion.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Companion.Models.Interface
 {
     public class Repository
     {
-        // in progress
+        private readonly ApplicationContext db;
+        private readonly UserManager<User> userManager;
+        public Repository(ApplicationContext db, UserManager<User> userManager)
+        {
+            this.db = db;
+            this.userManager = userManager;
+        }
+
+        public async Task<IdentityResult> CreateUser(User user, string password = null)
+        {
+            var result = await userManager.CreateAsync(user, password);
+            return result;
+        }
+
+        public async Task<User> FindUser(string userId)
+        {
+            var result = await userManager.FindByIdAsync(userId);
+            return result;
+        }
+
+        public async Task<IdentityResult> UpdateUser(User user)
+        {
+            var result = await userManager.UpdateAsync(user);
+            return result;
+        }
+
+        public async Task<IdentityResult> DeleteUser(User user)
+        {
+            var result = await userManager.DeleteAsync(user);
+            return result;
+        }
+
+        public async Task<User> FindUserByName(string name)
+        {
+            var user = await userManager.FindByNameAsync(name);
+            return user;
+        }
+
+        public async Task AddLogin(User user, ExternalLoginInfo info)
+        {
+            await userManager.AddLoginAsync(user, info);
+        }
+
+        public List<User> GetUsers()
+        {
+            return userManager.Users.ToList();
+        }
+
+        public async Task<User> GetUser(ClaimsPrincipal user)
+        {
+            return await userManager.GetUserAsync(user);
+        }
+
+        public async Task CreateActivity(Activity activity)
+        {
+            db.Activity.Add(activity);
+            await db.SaveChangesAsync();
+        }
+
+        public async Task DeleteAcivity(int id)
+        {
+            var activity = db.Activity.Where(x => x.Id == id)
+                .FirstOrDefault();
+            db.Activity.Remove(activity);
+            await db.SaveChangesAsync();
+        }
+
+        public Activity GetActivity(int id)
+        {
+            var activity = db.Activity.Where(x => x.Id == id)
+                .FirstOrDefault();
+            return activity;
+        }
+
+        public List<Activity> GetActivities()
+        {
+            return db.Activity.ToList();
+        }
+
+        public async Task CreateConfig(Config config)
+        {
+            db.Config.Add(config);
+            await db.SaveChangesAsync();
+        }
+
+        public async Task CreateService(Service service)
+        {
+            db.Service.Add(service);
+            await db.SaveChangesAsync();
+        }
+
+        public async Task CreateLocation(Location location)
+        {
+            db.Location.Add(location);
+            await db.SaveChangesAsync();
+        }
+
+        public Location GetLocation(int id)
+        {
+            return db.Location.Where(x => x.Id == id)
+                .FirstOrDefault();
+        }
+
+        public List<Location> GetLocations()
+        {
+            return db.Location.ToList();
+        }
     }
 }
